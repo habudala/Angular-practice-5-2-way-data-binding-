c3b1d379734c463cf25f80633ed82801a87cfde2
habudala <habudala@live.com>
2016-03-30 23:27:03 -0400

    Two Way Data binding Practice
    Practicing two-way data binding with ng-model in Angular.

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+// creating a module called myModule
+var variable = angular.module("myModule",[]);
+
+
+
+												// // creating a controller called myController
+												// var myController  function($scope){
+												// 	$scope.message = "Angular js tutorial";
+												// }
+												// //registering controller with module
+												// myApp.controller("myController", myController);
+
+
+
+//or alternatively, I can do the above two lines in one>>>
+variable.controller("myController", function($scope){
+	//bellow I'm adding a property of message to the scope object (in the view)
+	// $scope.message = "Hello there ..."
+
+
+
+	//Let's work with some more complex data types than just a message... objects for instance!
+	var employee = {firstName: "Ben", lastName: "Hastings", gender: "Male"};
+
+	$scope.employee = employee;
+
+
+
+});
